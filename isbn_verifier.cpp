ab0c333068072fe37fc368a6e0972ced48f4d6dd
tkalwad1 <146018909+tkalwad1@users.noreply.github.com>
2024-05-22 11:55:18 -0400

    Update isbn_verifier.cpp

--- isbn_verifier.cpp
+++ isbn_verifier.cpp
@@ -6,6 +6,16 @@
 using namespace std;
 
 namespace isbn_verifier {
+    int firstDigit(int n) 
+        { 
+            // Remove last digit from number 
+            // till only one digit is left 
+            while (n >= 10)  
+                n /= 10; 
+              
+            // return the first digit 
+            return n; 
+        } 
     bool is_valid(string isbn){
         //first remove dashes
         isbn.erase(remove(isbn.begin(), isbn.end(), '-'), isbn.end()); 
@@ -25,6 +35,7 @@ namespace isbn_verifier {
             digits.push_back(myInteger % 10);
             myInteger /= 10;
         }
+        reverse(digits.begin(), digits.end());
 
         //account for X being converted to 1 and 0
         if(digits[digits.size() - 1] == 0 && digits[digits.size() - 2] == 1 && digits.size() != 10){
@@ -32,6 +43,8 @@ namespace isbn_verifier {
             digits.pop_back();
             digits.push_back(10);
         }
+
+        digits[0] = firstDigit(myInteger); //accounts for incorrect first digit
 
         //now apply formula
         if((digits[0] * 10 + digits[1] * 9 + digits[2] * 8 + digits[3] * 7 + digits[4] * 6 + digits[5] * 5 + digits[6] * 4 + digits[7] * 3 + digits[8] * 2 + digits[9] * 1) % 11 == 0){
