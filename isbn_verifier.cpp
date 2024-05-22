90fae4992937b5e9853909681b977f3cf110fb4a
tkalwad1 <146018909+tkalwad1@users.noreply.github.com>
2024-05-22 12:50:38 -0400

    Update isbn_verifier.cpp

--- isbn_verifier.cpp
+++ isbn_verifier.cpp
@@ -24,7 +24,7 @@ namespace isbn_verifier {
 
         //now check if contains X
         if(isbn[isbn.size() - 1] == 'X'){
-            isbn[isbn.size() - 1] = 10; // converts X to 10 (this is arbitrary)
+            isbn[isbn.size() - 1] = '9'; // converts X to 9 (this is arbitrary)
         }
 
         int myInteger = stoi(isbn); // converts string to int
@@ -37,9 +37,8 @@ namespace isbn_verifier {
         }
         reverse(digits.begin(), digits.end());
 
-        //account for X being converted to 1 and 0
-        if(digits[digits.size() - 1] == 0 && digits[digits.size() - 2] == 1 && digits.size() != 10){
-            digits.pop_back();
+        //account for X being converted to 9
+        if(digits[digits.size() - 1] == 9){
             digits.pop_back();
             digits.push_back(10);
         }
@@ -47,7 +46,7 @@ namespace isbn_verifier {
         digits[0] = firstDigit(myInteger); //accounts for incorrect first digit
 
         //now apply formula
-        if((digits[0] * 10 + digits[1] * 9 + digits[2] * 8 + digits[3] * 7 + digits[4] * 6 + digits[5] * 5 + digits[6] * 4 + digits[7] * 3 + digits[8] * 2 + digits[9] * 1) % 11 == 0){
+        if((digits[0] * 10 + digits[1] * 9 + digits[2] * 8 + digits[3] * 7 +               digits[4] * 6 + digits[5] * 5 + digits[6] * 4 + digits[7] * 3 + digits[8]          * 2 + digits[9] * 1) % 11 == 0){
             return true;
         }
         
